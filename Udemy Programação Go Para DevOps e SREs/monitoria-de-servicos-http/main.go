0f56391b8b02515b50c65f9d25b780cbbdc618ca
Victor Barreto <victorbarretolins@gmail.com>
2024-04-29 22:38:51 -0300

    feat: criando um monitor basico de http

---
+++ Udemy Programação Go Para DevOps e SREs/monitoria-de-servicos-http/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	agora := time.Now()
+	url := os.Args[1]
+
+	get, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Ocorreu um erro ao executar o get(url)")
+		panic(err)
+	}
+
+	decorrido := time.Since(agora).Seconds()
+	status := get.StatusCode
+	fmt.Printf("Status: [%d] Tempo de carga: [%f]\n", status, decorrido)
+}
