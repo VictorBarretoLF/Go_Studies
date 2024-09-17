a8c71e09887cfcc0af3381ca8e9f243c85aab1d6
Victor Barreto <victorbarretolins@gmail.com>
2024-09-17 15:44:01 -0300

    feat: variables scope

--- full_stack_go_programming/variables/scope_of_variables/main.go
+++ full_stack_go_programming/variables/scope_of_variables/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+var globalVariable int = 100
+
+var variable1 int = 200
+
 func main() {
-	
+	var localVariable int = 200
+
+	fmt.Printf("The value of globalVariable is %d\n", globalVariable)
+
+	fmt.Printf("The value of localVariable is %d\n\n", localVariable)
+
+	display()
+
+	var variable1 int = 500
+	fmt.Printf("The value of variable1 is %d\n", variable1)
+}
+
+func display() {
+	fmt.Printf("The value of globalVariable is %d\n\n", globalVariable)
 }
